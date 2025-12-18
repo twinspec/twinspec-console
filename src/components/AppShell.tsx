8fbb988e87d3a7349615c72bd8c0459cbf7a152d
Tajah Trapier <tajah.trapier@gmail.com>
2025-12-17 19:42:45 -0500

    Updated AppShell.tsx

---
+++ src/components/AppShell.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Sidebar } from "./Sidebar";
+import { Topbar } from "./Topbar";
+
+export function AppShell({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex min-w-0 flex-1 flex-col">
+        <Topbar />
+        <div className="min-w-0 flex-1">{children}</div>
+      </div>
+    </div>
+  );
+}
